3874e3cb47774096c3a61f28963b6f50973d1794
Arik <genkiberi@gmail.com>
2025-07-17 13:20:29 -0700

    Sync from Obsidian - 0 new, 0 updated, 0 deleted

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "My Digital Garden",
+    pageTitle: "My Study Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
